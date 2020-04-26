82085116f696fbc16d38ef008b3321545c3b7b23
Santiago Vera <santiago.vera@devsu.com>
2020-04-25 19:54:14 -0500

    Email auth validation implemented

--- app/src/main/java/com/jetbrains/handson/mpp/todoapp/AppLauncherActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/todoapp/AppLauncherActivity.kt
@@ -8,7 +8,10 @@ import android.widget.TextView
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import service.MessageDialog
 
 
 const val EXTRA_MESSAGE = "Launching Main Activity"
@@ -38,13 +41,7 @@ class AppLauncherActivity : AppCompatActivity() {
             val response = IdpResponse.fromResultIntent(data)
             if(resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this, ""+user!!.email, Toast.LENGTH_SHORT).show()
-                val editText = findViewById<TextView>(R.id.title_launcher)
-                val message = editText.text.toString()
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra(EXTRA_MESSAGE, message)
-                }
-                startActivity(intent)
+                checkIfEmailVerified(user!!)
             } else {
                 Toast.makeText(this, ""+response!!.error!!.message, Toast.LENGTH_SHORT).show()
             }
@@ -56,5 +53,47 @@ class AppLauncherActivity : AppCompatActivity() {
             .setAvailableProviders(providers)
             .setTheme(R.style.LoginTheme)
             .build(), MY_REQUEST_CODE)
+    }
+
+    private fun checkIfEmailVerified( user: FirebaseUser) {
+        if(user.isEmailVerified){
+            Toast.makeText(this, ""+user.email, Toast.LENGTH_SHORT).show()
+            val editText = findViewById<TextView>(R.id.title_launcher)
+            val message = editText.text.toString()
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+            startActivity(intent)
+        }
+        else{
+            val messageDialog = MessageDialog(this)
+            messageDialog.showMessage("Verification email needed. Please verify account. Email will be sent")
+            sendVerificationEmail(user)
+        }
+    }
+
+    private fun sendVerificationEmail(user: FirebaseUser) {
+        user.sendEmailVerification()
+            .addOnCompleteListener(OnCompleteListener { task ->
+                when {
+                    task.isSuccessful -> {
+                        FirebaseAuth.getInstance().signOut()
+                        val intent = Intent(this, this::class.java).apply {
+                            putExtra(EXTRA_MESSAGE, "Welcome")
+                        }
+                        startActivity(intent)
+                        finish()
+                    }
+                    else -> {
+                        overridePendingTransition(0,0)
+                        finish()
+                        overridePendingTransition(0,0)
+                        val intent = Intent(this, this::class.java).apply {
+                            putExtra(EXTRA_MESSAGE, "Error when sending Email")
+                        }
+                        startActivity(intent)
+                    }
+                }
+            })
     }
 }
